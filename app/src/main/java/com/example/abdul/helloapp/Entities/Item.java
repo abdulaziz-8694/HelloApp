c262a457da3c6b71f495ca810d33fc10eeb1e9af
abdulaziz-8694 <cs12b1001@iith.ac.in>
2017-05-15 16:04:01 +0530

    Adding gson, and recycler view dependencies

---
+++ app/src/main/java/com/example/abdul/helloapp/Entities/Item.java
@@ -0,0 +1,32 @@
+package com.example.abdul.helloapp.Entities;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Item implements Parcelable {
+
+
+    protected Item(Parcel in) {
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Item> CREATOR = new Creator<Item>() {
+        @Override
+        public Item createFromParcel(Parcel in) {
+            return new Item(in);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
+}
--- app/src/main/java/com/example/abdul/helloapp/ItemListActivity.java
+++ app/src/main/java/com/example/abdul/helloapp/ItemListActivity.java
@@ -3,8 +3,8 @@ package com.example.abdul.helloapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ItemList extends AppCompatActivity {
-
+public class ItemListActivity extends AppCompatActivity {
+    public static final String ITEM_URL = "https://gist.githubusercontent.com/maclir/f715d78b49c3b4b3b77f/raw/8854ab2fe4cbe2a5919cea97d71b714ae5a4838d/items.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
