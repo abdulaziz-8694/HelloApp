8a538bad5e09134ff3ccbff4a0b2d7ebd6375b90
abdulaziz-8694 <cs12b1001@iith.ac.in>
2017-05-15 16:28:51 +0530

    Implemented Network Manager as a singleton

--- app/src/main/java/com/example/abdul/helloapp/ItemListActivity.java
+++ app/src/main/java/com/example/abdul/helloapp/ItemListActivity.java
@@ -3,6 +3,10 @@ package com.example.abdul.helloapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Main Activity that fetches the data from the network once and
+ * shows the items as a list.
+ */
 public class ItemListActivity extends AppCompatActivity {
     public static final String ITEM_URL = "https://gist.githubusercontent.com/maclir/f715d78b49c3b4b3b77f/raw/8854ab2fe4cbe2a5919cea97d71b714ae5a4838d/items.json";
     @Override
